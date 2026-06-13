6f199513b1f0d347943a596b812e99b48e813e73
Idahk19 <idahkarwitha9@gmail.com>
2026-06-14 00:25:53 +0300

    Create an expense.js file and create a class Expense in it

---
+++ expense.js
@@ -0,0 +1,9 @@
+class Expense{
+    constructor(description, amount, category){
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+  
+    }
+}
+module.exports = Expense;
