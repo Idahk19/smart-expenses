d9ecab62f8a7b1c17db59877239e5d86c0a13524
Idahk19 <idahkarwitha9@gmail.com>
2026-06-14 02:17:39 +0300

    add the delete expense button and its function

--- script.js
+++ script.js
@@ -26,12 +26,23 @@ addBtn.addEventListener("click", () => {
 
 function updateUI(){
     list.innerHTML = ""; // clear old list first
-    tracker.expenses.forEach((expense) =>{
+    tracker.expenses.forEach((expense, index) =>{
         list.innerHTML += `
-    <li>
-        ${expense.description} - ${expense.amount} (${expense.category})
-    </li>
-`;
+            <li>
+                ${expense.description} - ${expense.amount} (${expense.category})
+                <button onclick="deleteExpense(${index})"
+                style="background-color: red; color: white; padding: 4px 8px; border: none; border-radius: 4px; cursor: pointer; margin-left: 10px;">
+                    Delete
+                </button>
+            </li>
+        `;
     });
     total.textContent = tracker.calculateTotal();
+}
+window.deleteExpense = function(index) {
+    const confirmDelete = confirm("Are you sure?");
+    if (confirmDelete) {
+        tracker.removeExpense(index);
+        updateUI();
+    }
 }
