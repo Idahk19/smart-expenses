1f6051095412f1f6503ad69741ad27b182ba7e6e
Idahk19 <idahkarwitha9@gmail.com>
2026-06-14 01:06:15 +0300

    Use DOM to get html elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const Expense = require('./expense.js')
+const ExpenseTracker = require ('./expenseTracker.js')
+
+const addBtn = document.getElementById("addBtn");
+const description = document.getElementById("description");
+const amount = document.getElementById("amount");
+const category = document.getElementById("category");
+const list = document.getElementById("expenseList");
+const total = document.getElementById("total");
